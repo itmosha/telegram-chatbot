710e482564aea4c8cfed14e1438031c66a1911cd
itmosha <itmosha0@gmail.com>
2022-10-12 14:17:10 +0300

    added: messages

--- source/funcs.py
+++ source/funcs.py
@@ -7,3 +7,11 @@ def get_token():
     f.close()
 
     return tmp["token"]
+
+
+def get_answers():
+    f = open('answers.json')
+    tmp = ujson.load(f)
+    f.close()
+
+    return tmp
--- source/main.py
+++ source/main.py
@@ -1,21 +1,21 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from funcs import get_token
-
+from funcs import get_token, get_answers
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=get_token())
 dp = Dispatcher(bot)
+answers = get_answers()
 
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.answer('Welcome!')
+    await message.answer(answers["START"])
 
 
 @dp.message_handler()
 async def process_message(message: types.Message):
-    await message.answer(f'Got a message: {message.text}')
+    await message.answer(answers["DEFAULT"] + message.text)
 
 
 if __name__ == '__main__':
